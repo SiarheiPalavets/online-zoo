49af956635a91bcaeec43395d94c6bc6188e9d40
Siarhei <palavetssiarhei@gmail.com>
2022-10-31 23:18:22 +0100

    init: start task

---
+++ pages/main/script.js
@@ -0,0 +1,8 @@
+let btns = header.getElementsByClassName("active-link");
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  let current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
